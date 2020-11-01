4e5fb3a27fc012d87cf713d98adbde449abf8c16
Wylliam Borba Bassos <bassoscolorado@gmail.com>
2020-11-01 11:21:55 -0300

    V1.0
    Finalizado Desafio-2

---
+++ src/App.js
@@ -0,0 +1,67 @@
+import React, { useState, useEffect } from "react";
+import Form from "./components/Form";
+import Repositories from "./components/Repositories";
+import api from "./services/api";
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      const apiRepositpries = response.data;
+      setRepositories(apiRepositpries)
+    })
+  }, [])
+
+  async function handleAddRepository(repository) {
+    const now = Date.now()
+    if(!repository)
+      repository = {
+        title: `Novo Repositprio ${now}`,
+        url: `https://github.com/wyllbassos/${now}`,
+        techs: ["NodeJS", "ReactJS", "ReactNative"]
+      }
+    const response = await api.post('repositories', repository)
+    setRepositories([...repositories, response.data])
+  }
+
+  async function handleRemoveRepository(id) {
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    if(repositoryIndex >= 0){
+      const response = await api.delete(`repositories/${id}`)
+      if(response.status === 204){
+        const newRepositories = repositories.filter(repository => repository.id !== id)
+        console.log(newRepositories)
+        setRepositories(newRepositories)
+      }
+    }
+  }
+
+  async function handleAddLike(id){
+    const repositoryIndex = repositories.findIndex(repository => repository.id === id);
+    if(repositoryIndex >= 0){
+      const response = await api.post(`repositories/${id}/like`)
+      if(response.data){
+        const newRepositories = [...repositories]
+        newRepositories[repositoryIndex].likes = response.data.likes
+        setRepositories(newRepositories)
+      }
+    }
+  }
+
+  return (
+    <div>
+      <Repositories 
+        repositories={repositories}
+        handles={{handleRemoveRepository, handleAddLike}}
+      />
+      <Form
+        handles={{ handleAddRepository }}
+      />
+    </div>
+  );
+}
+
+export default App;
